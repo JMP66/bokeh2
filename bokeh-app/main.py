23dc71454e7b848647415654d6221e3fd84fc425
nyc-public-schools <64694595+nyc-public-schools@users.noreply.github.com>
2020-05-06 17:10:12 +0200

    Update main.py

--- bokeh-app/main.py
+++ bokeh-app/main.py
@@ -39,17 +39,16 @@ for num in nums:
 
 
 def geographic_to_web_mercator(x_lon, y_lat):     
-    if abs(x_lon) <= 180 and abs(y_lat) < 90:          
-        num = x_lon * 0.017453292519943295         
-        x = 6378137.0 * num         
-        a = y_lat * 0.017453292519943295          
-        x_mercator = x         
-        y_mercator = 3189068.5 * np.log((1.0 + np.sin(a)) / (1.0 - np.sin(a))) 
-     else:
-        x_mercator = 0
-        y_mercator = 0
-     
-     return x_mercator, y_mercator  
+       if abs(x_lon) <= 180 and abs(y_lat) < 90:          
+              num = x_lon * 0.017453292519943295   
+              x = 6378137.0 * num  
+              a = y_lat * 0.017453292519943295  
+              x_mercator = x    
+              y_mercator = 3189068.5 * np.log((1.0 + np.sin(a)) / (1.0 - np.sin(a))) 
+        else:         
+              x_mercator = 0
+              y_mercator = 0
+       return x_mercator, y_mercator  
 
 #Ger mercator coordinates
 mercx = []
