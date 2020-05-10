46d520314c6525eca819838caf18df3b416fd385
nyc-public-schools <64694595+nyc-public-schools@users.noreply.github.com>
2020-05-10 19:04:30 +0200

    Update main.py

--- bokeh-app/main.py
+++ bokeh-app/main.py
@@ -44,7 +44,10 @@ def geographic_to_web_mercator(x_lon, y_lat):
         x = 6378137.0 * num         
         a = y_lat * 0.017453292519943295          
         x_mercator = x         
-        y_mercator = 3189068.5 * np.log((1.0 + np.sin(a)) / (1.0 - np.sin(a)))         
+        y_mercator = 3189068.5 * np.log((1.0 + np.sin(a)) / (1.0 - np.sin(a)))  
+    else: 
+        x_mercator = 0
+        y_mercator = 0
         
         return x_mercator, y_mercator  
 
